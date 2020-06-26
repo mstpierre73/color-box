98c15a53dbdeb68d59b5ce4c41aecaaff873f26f
Marise St-Pierre <naranjaqc@hotmail.com>
2020-06-26 16:12:40 -0400

    first commit

---
+++ src/Box.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import './Box.css';
+
+class Box extends Component {
+    constructor(props){
+        super(props);
+        this.state = {color: 'purple'};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        console.log('fonctionne!')
+    }
+    
+    render() {
+        return (
+            <div 
+            className='Box' 
+            style={{backgroundColor: this.state.color}}
+            onClick={this.handleClick}
+            />
+        );
+    }
+}
+
+export default Box; 
